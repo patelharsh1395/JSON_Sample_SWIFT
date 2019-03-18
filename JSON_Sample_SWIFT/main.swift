17bf906600205c6d1be8729670e97c19448f63d0
Harsh <harsh1395@Harshs-MacBook-Pro.local>
2019-03-18 11:36:46 -0400

    Initial Commit

---
+++ JSON_Sample_SWIFT/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSON_Sample_SWIFT
+//
+//  Created by Harsh on 2019-03-18.
+//  Copyright © 2019 Harsh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
