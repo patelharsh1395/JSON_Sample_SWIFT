75112573805dc7e73270597275c19a506ac7785a
Harsh <harsh1395@Harshs-MacBook-Pro.local>
2019-03-18 14:10:17 -0400

    ......

--- JSON_Sample_SWIFT/main.swift
+++ JSON_Sample_SWIFT/main.swift
@@ -8,5 +8,47 @@
 
 import Foundation
 
-print("Hello, World!")
+var jsonString = """
+{
+"name" : "Harsh",
+"rollno" : 30
+}
+""".data(using: .utf8)!
+
+class Student : Codable
+{
+    var Fname : String?
+    var rollno : Int?
+    
+    enum CodingKeys : String , CodingKey {
+        case Fname = "name"
+        case rollno
+    }
+    
+}
+
+
+
+func jsonfunc()
+{
+  guard  let url = URL(string: "https://api.myjson.com/bins/6rovy")
+    else
+    {
+    return
+    }
+    print(url)
+    print("inside func ")
+    URLSession.shared.dataTask(with: url) { (data, resp, er) in
+      print("inside block")
+    
+    
+    }.resume()
+
+
+}
+
+jsonfunc()
+
+
+
 
